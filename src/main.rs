c6ca07b748dbfeb2070ff43a342da15dccdd1468
Stéphane Bisinger <stephane@sbisinger.ch>
2024-10-31 00:11:42 +0100

    Add more structures and convert the result into JSON

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use binrw::io::{BufReader, Cursor};
 use binrw::{binrw, BinReaderExt, BinResult, BinWriterExt, NullString};
 use clap::Parser;
+use serde::Serialize;
 use std::fs::File;
 
 #[derive(Parser)]
@@ -15,8 +16,8 @@ fn main() {
         println!("File not found");
         std::process::exit(-1);
     }
-    println!("Parsing {}", args.file.to_str().unwrap_or("<missing>"));
-    read_file(&args.file);
+    let parsed_header = read_file(&args.file).unwrap();
+    println!("{}", serde_json::to_string_pretty(&parsed_header).unwrap());
 }
 
 #[binrw]
@@ -28,17 +29,10 @@ pub struct EncodedHeader {
     log_version: u32,
 }
 
-#[derive(Debug)]
 #[binrw]
-pub struct RecHeader {
-    game: NullString,
-    save: f32,
-    version_minor: u16,
-    version_major: u16,
-    build: u32,
-    timestamp: i32,
-    version_2: [u16; 2],
-    interval_version: [u16; 2],
+#[derive(Serialize)]
+#[br(import(major: u16))]
+pub struct GameSettings {
     game_options_version: u32,
     n_dlc: u32,
     #[br(count=n_dlc)]
@@ -88,6 +82,7 @@ pub struct RecHeader {
     fog_of_war: Bool,
     cheat_notifications: Bool,
     colored_chat: Bool,
+    #[serde(skip_serializing)]
     #[br(count = 8 - n_players)]
     empty_slots: Vec<EmptySlot>,
     #[br(magic = b"\xa3_\x02\x00")]
@@ -101,121 +96,256 @@ pub struct RecHeader {
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     rms_strings: Vec<DeString>,
+    #[serde(skip_serializing)]
     unknown2: u32,
+    #[serde(skip_serializing)]
     unknown3: u32,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
-    other_strings1: Vec<DeString>,
+    other_strings: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings2: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings3: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings4: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings5: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings6: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings7: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings8: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings9: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings10: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings11: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings12: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings13: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings14: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings15: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings16: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings17: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings18: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings19: Vec<DeString>,
+    #[serde(skip_serializing)]
     #[br(parse_with = read_strings_of_length)]
     #[bw(write_with = write_len_and_string)]
     other_strings20: Vec<DeString>,
+    #[serde(skip_serializing)]
     num_strategic_numbers: u32,
     #[br(count = num_strategic_numbers)]
     strategic_numbers: Vec<i32>,
     num_ai_files: u32,
     #[br(count = num_ai_files)]
     ai_files: Vec<AIFile>,
+    #[serde(skip_serializing)]
     unknown4: u32, // 25.02
+    #[serde(skip_serializing)]
     unknown5: u32, // 25.02
+    #[serde(skip_serializing)]
     unknown6: u32, // 25.02
     guid: [u8; 16],
     lobby_name: DeString,
+    #[serde(skip_serializing)]
     unknown7: [u8; 8], // 25.22
     modded_dataset: DeString,
+    #[serde(skip_serializing)]
     unknown8: [u8; 19],
-    unknown9: [u8; 5],   // 13.13
-    unknown10: [u8; 3],  // 13.17
+    #[serde(skip_serializing)]
+    unknown9: [u8; 5], // 13.13
+    #[serde(skip_serializing)]
+    unknown10: [u8; 3], // 13.17
+    #[serde(skip_serializing)]
     unknown11: DeString, // 13.17
-    unknown12: [u8; 3],  // 13.17
-    unknown13: u8,       // 20.06
-    unknown14: [u8; 8],  // 20.16
+    #[serde(skip_serializing)]
+    unknown12: [u8; 3], // 13.17
+    #[serde(skip_serializing)]
+    unknown13: u8, // 20.06
+    #[serde(skip_serializing)]
+    unknown14: [u8; 8], // 20.16
+    #[serde(skip_serializing)]
     unknown15: [u8; 21], // 25.06
-    unknown16: [u8; 4],  // 25.22
-    unknown17: [u8; 8],  // 26.16
-    unknown18: [u8; 3],  // 37
-    unknown19: [u8; 8],  // 50
-    #[br(if(version_major >= 63))]
+    #[serde(skip_serializing)]
+    unknown16: [u8; 4], // 25.22
+    #[serde(skip_serializing)]
+    unknown17: [u8; 8], // 26.16
+    #[serde(skip_serializing)]
+    unknown18: [u8; 3], // 37
+    #[serde(skip_serializing)]
+    unknown19: [u8; 8], // 50
+    #[serde(skip_serializing)]
+    #[br(if(major >= 63))]
     unknown24: Option<[u8; 5]>,
+    #[serde(skip_serializing)]
     unknown20: DeString,
+    #[serde(skip_serializing)]
     unknown21: [u8; 5],
-    unknown22: u8,      // 13.13
+    #[serde(skip_serializing)]
+    unknown22: u8, // 13.13
+    #[serde(skip_serializing)]
     unknown23: [u8; 2], // 13.13
     ver37: [u32; 2],
 }
 
-#[derive(Debug)]
 #[binrw]
+#[derive(Serialize)]
+pub struct AIConfig {
+    has_ai: u32,
+    // TODO: Implement the case where the AI is present
+}
+
+#[binrw]
+#[derive(Serialize)]
+pub struct Replay {
+    old_time: u32,
+    world_time: u32,
+    old_world_time: u32,
+    game_speed_id: u32,
+    world_time_delta_seconds: u32,
+    timer: f32,
+    game_speed: f32,
+    temp_pause: Bool,
+    next_object_id: u32,
+    next_reusable_object_id: i32,
+    random_seed: u32,
+    random_seed_2: u32,
+    rec_player: u16,
+    num_players: u8,
+    instant_build: Bool,
+    cheats_enabled: Bool,
+    game_mode: u16,
+    campaign: u32,
+    campaign_player: u32,
+    campaign_scenario: u32,
+    king_campaign: u32,
+    king_campaign_player: u8,
+    king_campaign_scenario: u8,
+    player_turn: u32,
+    #[br(count = num_players)]
+    player_turns: Vec<u32>,
+    #[serde(skip_serializing)]
+    padding: [u8; 8],
+}
+
+#[binrw]
+#[derive(Serialize)]
+pub struct RecHeader {
+    game: MyNullString,
+    save: f32,
+    version_minor: u16,
+    version_major: u16,
+    build: u32,
+    timestamp: i32,
+    version_2: [u16; 2],
+    interval_version: [u16; 2],
+    #[br(args(version_major))]
+    game_settings: GameSettings,
+    ai_config: AIConfig,
+    replay: Replay,
+    map_info: MapInfo,
+}
+
+#[binrw]
+#[derive(Serialize)]
+#[br(import(tile_count: u32))]
+pub struct IgnoreMapTile {
+    tile_num: u32,
+    #[serde(skip_serializing)]
+    unknown1: [u8; 2044],
+    #[serde(skip_serializing)]
+    #[br(count = tile_count)]
+    unknown_tiles: Vec<u16>,
+    float_count: u32,
+    #[serde(skip_serializing)]
+    #[br(count = float_count)]
+    unknown2: Vec<f32>,
+    #[serde(skip_serializing)]
+    unknown3: u32,
+}
+
+#[binrw]
+#[derive(Serialize)]
+pub struct MapInfo {
+    size_x: u32,
+    size_y: u32,
+    zone_count: u32,
+    #[br(args { inner: (size_x * size_y,)} )]
+    #[br(count = zone_count)]
+    ignored_map_tiles: Vec<IgnoreMapTile>,
+    all_visible: Bool,
+    fog_of_war: Bool,
+}
+
+#[binrw]
+#[derive(Serialize)]
 pub struct Player {
     dlc_id: u32,
     color_id: i32,
     selected_color: u8,
     selected_team_id: u8,
     resolved_team_id: u8,
+    #[serde(skip_serializing)]
     dat_crc: [u8; 8],
     mp_game_version: u8,
     civ_id: u32,
-    unknown: u32,
+    custom_civ_count: u32,
+    #[br(count = custom_civ_count)]
+    custom_civ_ids: Vec<u32>,
     ai_type: DeString,
     ai_civ_name_index: u8,
     ai_name: DeString,
     name: DeString,
     player_type: u32,
     profile_id: u32,
-    // #[br(magic = b"\x00\x00\x00\x00")]
     ai: [u8; 4],
     player_number: i32,
     prefer_random: u8,
@@ -223,12 +353,12 @@ pub struct Player {
     handicap: [u8; 8],
 }
 
-#[derive(Debug)]
 #[binrw]
+#[derive(Serialize)]
 struct EmptySlot {
     i0x: u32,
     i0a: u32,
-    unknown1: u32,
+    i0b: u32,
     unknown2: u32,
     s1: DeString,
     a2: u8,
@@ -240,8 +370,8 @@ struct EmptySlot {
     a4: [u8; 8],
 }
 
-#[derive(Debug)]
 #[binrw]
+#[derive(Serialize)]
 struct AIFile {
     unknown: u32,
     name: DeString,
@@ -261,6 +391,15 @@ impl std::fmt::Debug for Bool {
     }
 }
 
+impl Serialize for Bool {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_bool(self.value)
+    }
+}
+
 #[binrw]
 pub struct DeString {
     #[br(magic = b"\x60\x0A")]
@@ -269,31 +408,46 @@ pub struct DeString {
     value: Vec<u8>,
 }
 
+#[binrw]
+pub struct MyNullString {
+    text: NullString,
+}
+
 impl std::fmt::Debug for DeString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", std::string::String::from_utf8_lossy(&self.value))
     }
 }
 
-fn read_file(path: &std::path::Path) -> std::io::Result<()> {
+impl Serialize for DeString {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let strvalue = std::string::String::from_utf8_lossy(&self.value);
+        serializer.serialize_str(&strvalue)
+    }
+}
+
+impl serde::Serialize for MyNullString {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let strvalue = std::string::String::from_utf8_lossy(&self.text);
+        serializer.serialize_str(&strvalue)
+    }
+}
+
+fn read_file(path: &std::path::Path) -> std::io::Result<RecHeader> {
     let file = File::open(path)?;
     let mut reader = BufReader::new(file);
     let encoded_header: EncodedHeader = reader.read_le().unwrap();
-    println!(
-        "Header length: {} {}",
-        encoded_header.length, encoded_header.other
-    );
     let (header, _) = yazi::decompress(&encoded_header.zheader, yazi::Format::Raw).unwrap();
-    println!(
-        "First 8 bytes: {}, {}, {}, {}, {}, {}, {}, {}",
-        header[0], header[1], header[2], header[3], header[4], header[5], header[6], header[7]
-    );
-    println!("Log version: {}", encoded_header.log_version);
     let mut hreader = Cursor::new(header);
     let parsed_header: RecHeader = hreader.read_le().unwrap();
-    println!("{:?}", parsed_header);
 
-    Ok(())
+    Ok(parsed_header)
 }
 
 #[binrw::parser(reader, endian)]
@@ -301,7 +455,6 @@ fn read_strings_of_length() -> BinResult<Vec<DeString>> {
     let mut strings: Vec<DeString> = Vec::new();
     loop {
         let crc: u32 = reader.read_type(endian)?;
-        println!("crc: {}", crc);
         if crc > 0 && crc < 255 {
             break;
         }
